81222099fe8ee1d97b28ce0c9b07d69b2b5b0e71
A <A>
2020-05-29 19:27:46 +0200

    Initial Commit

---
+++ Home Work 2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home Work 2.4
+//
+//  Created by Anzhelika Pikulina on 29/05/2020.
+//  Copyright © 2020 Anzhelika Pikulina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
