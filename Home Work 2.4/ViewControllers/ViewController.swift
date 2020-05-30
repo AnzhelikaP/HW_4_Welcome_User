f8e8ecdbccac5d66243e6afa4df7f9aad9099ba9
A <A>
2020-05-30 02:25:57 +0200

    Alert Controller

--- Home Work 2.4/ViewControllers/ViewController.swift
+++ Home Work 2.4/ViewControllers/ViewController.swift
@@ -9,18 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var userNameTextFild: UITextField!
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func ForgotNameButton() {
+        showAlert(with: "", and: "Oops! \n Your name is User 😉")
+        return
+        
     }
     
     @IBAction func ForgotPasswordButton() {
+        showAlert(with: "", and: "Oops! \n Your password is Password 😉")
+        return
+        
     }
+    
 }
 
+
+// MARK: - UIAlertController
+extension ViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.userNameTextField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
